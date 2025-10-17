4cb9bbbda8a07596edee2966389feea046f7ecee
Edward Whitehead <48436230+ultracoolbru@users.noreply.github.com>
2025-10-17 16:09:17 +0400

    feat: add Facebook link to social media navigation bar

--- src/components/kyros/Topbar.tsx
+++ src/components/kyros/Topbar.tsx
@@ -9,7 +9,8 @@ export default function KyrosTopbar() {
                 <Link href="https://www.linkedin.com/in/edwardwhiteheaddev" target="_blank" rel="noopener noreferrer">LinkedIn</Link> |{' '}
                 <Link href="https://github.com/edwardwhiteheaddev" target="_blank" rel="noopener noreferrer">GitHub</Link> |{' '}
                 <Link href="https://x.com/edwhiteheaddev" target="_blank" rel="noopener noreferrer">X</Link> |{' '}
-                <Link href="https://www.instagram.com/edwardwhiteheaddev" target="_blank" rel="noopener noreferrer">Instagram</Link>
+                <Link href="https://www.instagram.com/edwardwhiteheaddev" target="_blank" rel="noopener noreferrer">Instagram</Link> |{' '}
+                <Link href="https://www.facebook.com/edwardwhiteheaddev" target="_blank" rel="noopener noreferrer">Facebook</Link>
             </span>
         </div>
     );
